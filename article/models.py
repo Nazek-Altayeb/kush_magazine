71c8206e3547d95887c6f387f471a07f26ad47f4
Nazek-Altayeb <>
2023-05-31 19:21:26 +0000

    create superuser

--- article/models.py
+++ article/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+"""class Article(models.Model):
+    title = models.CharField(max_length=100, null=False, blank=False)
+    body = models.CharField(max_length=1500, null=False, blank=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    topic = models.ForeignKey("Topic", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title + '|' + str(self.author)"""
