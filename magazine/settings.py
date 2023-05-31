e26736db8fc357c70c354bd1f6f36da28e726fdc
Nazek-Altayeb <>
2023-05-31 22:33:39 +0000

    update the navbar of base.html

--- magazine/settings.py
+++ magazine/settings.py
@@ -134,6 +134,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
+# login / logout REDIRECT
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
