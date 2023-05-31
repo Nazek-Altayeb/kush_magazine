193f5a05961875c932a222b7ab63c8ae33dede99
Nazek-Altayeb <>
2023-05-31 21:38:55 +0000

    add article functionality

--- article/models.py
+++ article/models.py
@@ -1,14 +1,31 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 
 
-"""class Article(models.Model):
+class Article(models.Model):
     title = models.CharField(max_length=100, null=False, blank=False)
     body = models.CharField(max_length=1500, null=False, blank=False)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    topic = models.ForeignKey("Topic", on_delete=models.CASCADE)
+    # topic = models.ForeignKey("Topic", on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.title + '|' + str(self.author)"""
+        return self.title + '|' + str(self.author)
+
+    def get_absolute_url(self):
+        return reverse('article', args=str(self.pk))
+
+
+"""class Comment(models.Model):
+    body = models.CharField(max_length=250, null=False, blank=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    article = models.ForeignKey("Article", on_delete=models.CASCADE)"""
+
+
+"""class Topic(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+
+    def __str__(self):
+        return self.name"""
--- article/views.py
+++ article/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+from .models import Article
 
 # Create your views here.
+
+
+class HomeView(ListView):
+    model = Article
+    template_name = 'home.html'
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'article.html'
+
+
+class AddArticleView(CreateView):
+    model = Article
+    template_name = 'add-article.html'
+    fields = '__all__'
