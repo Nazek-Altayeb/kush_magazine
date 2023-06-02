49f714082a60313e8222b376b7c673c902416c24
Nazek-Altayeb <>
2023-06-02 14:26:06 +0000

    fix typo in urls.py

--- article/urls.py
+++ article/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('article/<int:pk>', views.ArticleDetailView.as_view(), name='article'),
     path('add-article', views.AddArticleView.as_view(), name='add-article'),
     path('article/edit-article/<int:pk>',
-         views.UpdateArticleView.as_view(), name='edit-article')
+         views.UpdateArticleView.as_view(), name='edit-article'),
     path('article/delete-article/<int:pk>',
          views.DeleteArticleView.as_view(), name='delete-article')
 ]
