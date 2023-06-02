c358847a1dd00fcf175ce718ba7a1aa68ef08dbe
Nazek-Altayeb <>
2023-06-02 16:40:08 +0000

    add new attributes to form fields of ArticleForm

--- article/forms.py
+++ article/forms.py
@@ -7,10 +7,10 @@ class ArticleForm(forms.ModelForm):
         model = Article
         fields = ('title', 'body', 'author',)
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'body': forms.Textarea(attrs={'class': 'form-control'}),
+            'title': forms.TextInput(attrs={'class': 'form-control', required=True}),
+            'body': forms.Textarea(attrs={'class': 'form-control', required=True}),
             # 'author': forms.Select(attrs={'class': 'form-control'}),
-            'author': forms.Select(attrs={'class': 'form-control', 'value': '', 'id': 'user-id'})
+            'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'user-id', 'readonly': 'readonly'}),
         }
 
 
