86574895ce03b5a0863b74a29737d1301283b544
Nazek-Altayeb <>
2023-06-02 12:51:09 +0000

    create ArticleForm and update add-article.html

---
+++ article/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Article
+
+
+class ArticleForm(forms.ModelForm):
+    class Meta:
+        model = Article
+        fields = ('title', 'boby', 'author')
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+            'author': forms.Select(attrs={'class': 'form-control'})
+        }
