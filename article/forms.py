a261525cceae3d8eec18106524c6d321bc67959f
Nazek-Altayeb <>
2023-06-02 17:01:37 +0000

    display user name in add-article page

--- article/forms.py
+++ article/forms.py
@@ -10,7 +10,7 @@ class ArticleForm(forms.ModelForm):
             'title': forms.TextInput(attrs={'class': 'form-control', 'required': 'True'}),
             'body': forms.Textarea(attrs={'class': 'form-control', 'required': 'True'}),
             # 'author': forms.Select(attrs={'class': 'form-control'}),
-            'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'innerText': '', 'id': 'user-id', 'readonly': 'readonly'}),
+            'author': forms.TextInput(attrs={'class': 'form-control', 'value': '',  'id': 'user-id', 'readonly': 'readonly'}),
         }
 
 
