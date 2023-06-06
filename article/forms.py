f5e4d7dac1feea4bbf2e399235f7d2fa4ab06eb0
Nazek-Altayeb <>
2023-06-06 09:06:46 +0000

    clean code in forms.py

--- article/forms.py
+++ article/forms.py
@@ -10,14 +10,14 @@ for category in categories:
 class ArticleForm(forms.ModelForm):
     class Meta:
         model = Article
-        fields = ('title','author', 'body', )
+        fields = ('title', 'author', 'body', )
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'required': 'True'}),
             'author': forms.TextInput(attrs={'class': 'form-control', 'value': '',  'id': 'user-id', 'type': 'hidden'}),
             # 'category': forms.Select(choices=categories_list, attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control', 'required': 'True'}),
             # 'author': forms.Select(attrs={'class': 'form-control'}),
-            
+
         }
 
 
@@ -29,7 +29,7 @@ class EditArticleForm(forms.ModelForm):
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
         }
-        
+
 
 """class CategoryForm(forms.ModelForm):
     class Meta:
