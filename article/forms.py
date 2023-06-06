317448eb55aeaa3c633b00ed8cb5b2abab996624
Nazek-Altayeb <>
2023-06-06 09:26:04 +0000

    remove add-category link from base.html

--- article/forms.py
+++ article/forms.py
@@ -13,9 +13,9 @@ class ArticleForm(forms.ModelForm):
         fields = ('title', 'author', 'body', )
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'required': 'True'}),
-            'author': forms.TextInput(attrs={'class': 'form-control', 'value': '',  'id': 'user-id', 'type': 'hidden'}),
+            'author': forms.TextInput(attrs={'class': 'form-control', 'value':'',  'id': 'user-id', 'type': 'hidden'}),
             # 'category': forms.Select(choices=categories_list, attrs={'class': 'form-control'}),
-            'body': forms.Textarea(attrs={'class': 'form-control', 'required': 'True'}),
+            'body': forms.Textarea(attrs={'class': 'form-control', 'required':'True'}),
             # 'author': forms.Select(attrs={'class': 'form-control'}),
 
         }
