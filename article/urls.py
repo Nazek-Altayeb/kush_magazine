806e1666bf5c77ae5052ebf9552648c381f113be
Nazek-Altayeb <>
2023-06-07 10:48:09 +0000

    slugify category

--- article/urls.py
+++ article/urls.py
@@ -1,15 +1,13 @@
 from django.urls import path
-from .views import HomeView, ArticleDetailView, AddArticleView, UpdateArticleView, DeleteArticleView, AddCategoryView, CategoryView
+from . import views
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
-    path('article/<int:pk>', ArticleDetailView.as_view(), name='article'),
-    path('add-article', AddArticleView.as_view(), name='add-article'),
-    path('add-category', AddCategoryView.as_view(), name='add-category'),
-    path('article/edit-article/<int:pk>',
-         UpdateArticleView.as_view(), name='edit-article'),
-    path('article/delete-article/<int:pk>',
-         DeleteArticleView.as_view(), name='delete-article'),
-    path('category/<str:catg>', CategoryView, name='category')
+    path('', views.HomeView.as_view(), name='home'),
+    path('article/<int:pk>', views.ArticleDetailView.as_view(), name='article'),
+    path('add-article', views.AddArticleView.as_view(), name='add-article'),
+    path('add-category', views.AddCategoryView.as_view(), name='add-category'),
+    path('article/edit-article/<int:pk>', views.UpdateArticleView.as_view(), name='edit-article'),
+    path('article/delete-article/<int:pk>', views.DeleteArticleView.as_view(), name='delete-article'),
+    path('category/<str:catg>', views.CategoryView, name='category')
     # path('like/<int:pk>', LikeView, name='like_article')
 ]
