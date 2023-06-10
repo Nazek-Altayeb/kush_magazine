c6e43e5b21a3508b818c716cb4538126db02381f
Nazek-Altayeb <>
2023-06-10 11:21:12 +0000

    fix field name in CommentForm class

--- article/forms.py
+++ article/forms.py
@@ -42,7 +42,7 @@ class CategoryForm(forms.ModelForm):
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ("body",)
+        fields = ('body',)
         widget = {
             'body': forms.Textarea(attrs={'class': 'form-control'}),
         }
