b86e6e52243f43fecb8f291c9b8ff5f8232d4210
Nazek-Altayeb <>
2023-06-12 13:08:43 +0000

    update get_success_url in ArticleDetailView

--- article/views.py
+++ article/views.py
@@ -40,7 +40,7 @@ class ArticleDetailView(FormMixin, DetailView):
         return context
 
     def get_success_url(self):
-        return reverse('article', kwargs={'pk': self.object.id})
+        return HttpResponseRedirect(reverse('article', args=[str(pk)]))
 
     def post(self, request, *args, **kwargs):
         self.object = self.get_object()
