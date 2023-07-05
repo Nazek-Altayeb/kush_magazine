1cb6cea394db2b25b0404301f6ff232130364069
Nazek-Altayeb <>
2023-07-05 17:05:42 +0000

    change the debug environment

--- magazine/settings.py
+++ magazine/settings.py
@@ -27,8 +27,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-  # DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = True
 
 ALLOWED_HOSTS = ['kush-online-magazine.herokuapp.com', 'localhost', '8000-nazek-altayeb-kush-magaz-713fsmh5dk.us2.codeanyapp.com']
 
